9654c668dce45d63ccf4a41015efd103a0e34e64
camalot <camalot@gmail.com>
2017-01-28 11:11:42 -0600

    refactor to process the root files as well

--- Git-Secrets.ps1
+++ Git-Secrets.ps1
@@ -17,49 +17,54 @@ function Scan-Path {
 		$rules = Load-Rules -Path "./rules.json";
 	}
   process {
-		$children = @(Get-ChildItem -Path (Resolve-Path -Path $Path) -Recurse);
-		[System.Collections.ArrayList] $violations = @();
-    $children | foreach {
-      $item = $_;
-			$item | Write-Host;
-      if ( -not $item.PSIsContainer ) {
-				$content = Get-Content -Path $item.FullName;
-        $rules.patterns | foreach {
-					$pattern = $_;
-          $content | Select-String -Pattern $pattern -AllMatches | foreach { $_.Matches; } | foreach {
-						$match = $_
-						$m = $match.Groups[0].Value;
-            $result = "$($item.FullName): $($match)";
-						if ( $violations.IndexOf($result) -ge 0 ) {
-							continue;
+		try {
+			$children = @(Get-ChildItem -Path (Resolve-Path -Path $Path) -Recurse);
+			[System.Collections.ArrayList] $violations = @();
+			for($i = 0; $i -lt $children.Count; ++$i) {
+	    # $children | foreach {
+	      $item = $children[$i];
+	      if ( -not $item.PSIsContainer ) {
+					$content = Get-Content -Path $item.FullName;
+					for($y = 0; $y -lt $rules.patterns.Count; ++$y) {
+	        # $rules.patterns | foreach {
+						$pattern = $rules.patterns[$y];
+	          $content | Select-String -Pattern $pattern -AllMatches | foreach { $_.Matches; } | foreach {
+							$match = $_
+							$m = $match.Groups[0].Value;
+	            $result = "$($item.FullName): $($match)";
+							if ( $violations.IndexOf($result) -ge 0 ) {
+								continue;
+							}
+							$violations.Add($result) | Out-Null;
+	          };
+	        };
+	      } else {
+					#return Recursive-Scan -Path $item.FullName;
+	      }
+	    };
+			for($x = 0; $x -lt $violations.Count; ++$x) {
+				$v = $violations[$x];
+				$rules.allowed | foreach {
+					$allowed = $_;
+					if($v -match $allowed) {
+						$vidx = $violations.IndexOf($v);
+						if($vidx -ge 0) {
+							$violations.Remove($v) | Out-Null;
 						}
-						$violations.Add($result) | Out-Null;
-          };
-        };
-      } else {
-				#return Recursive-Scan -Path $item.FullName;
-      }
-    };
-
-		$violations | foreach {
-			$v = $_
-			$rules.allowed | foreach {
-				$allowed = $_;
-				if($v -match $allowed) {
-					$vidx = $violations.IndexOf($v);
-					if($vidx -ge 0) {
-						$violations.Remove($v) | Out-Null;
 					}
 				}
 			}
+			$violations | Write-Host;
+			if($violations.Count -gt 0) {
+				"`n[Error]: Found $($violations.Count) Violations.`n" | Write-Host -ForegroundColor DarkYellow;
+				"Possible mitigations:`n`t- Mark false positives as allowed adding exceptions to 'rules.json'`n`n" | Write-Host -ForegroundColor DarkYellow;
+				exit $violations.Count;
+			}
+	  } catch {
+			$_ | Write-Error;
+			exit 999;
 		}
-		$violations | Write-Host;
-		if($violations.Count -gt 0) {
-			"`n[Error]: Found $($violations.Count) Violations.`n" | Write-Host -ForegroundColor DarkYellow;
-			"Possible mitigations:`n`t- Mark false positives as allowed adding exceptions to 'rules.json'`n`n" | Write-Host -ForegroundColor DarkYellow;
-			exit $violations.Count;
-		}
-  }
+	}
 }
 
 Scan-Path -Path $Path;
